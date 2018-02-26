1e2cc4ace5534ddd70039414ba29ae679c1e5aba
王振辉{wangzhenhui} <wangzhenhui1991@users.noreply.github.com>
2018-02-26 23:41:22 +0800

    update

---
+++ chapater-sklearn/readme.py
@@ -0,0 +1,12 @@
+#--
+
+# Scikit learn 也简称 sklearn, 是机器学习领域当中最知名的 python 模块之一.
+#
+# Sklearn 包含了很多种机器学习的方式:
+#
+# Classification 分类
+# Regression 回归
+# Clustering 非监督分类
+# Dimensionality reduction 数据降维
+# Model Selection 模型选择
+# Preprocessing 数据预处理
